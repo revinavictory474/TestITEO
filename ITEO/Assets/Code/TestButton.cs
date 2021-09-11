8c04e8685c0ef7aae11d7d5ba88ab9edfef9c95b
Revina Victory <revina.victory@gmail.com>
2021-09-11 12:03:29 +0700

    interactive panel image lorem ipsum

--- ITEO/Assets/Code/TestButton.cs
+++ ITEO/Assets/Code/TestButton.cs
@@ -7,6 +7,7 @@ public class TestButton : MonoBehaviour
 {
     public GameObject thisGameObj;
     public Transform camera;
+    public GameObject uiPanel;
     private float speed = 0.03f;
     public Vector3 offset;
 
@@ -15,6 +16,7 @@ public class TestButton : MonoBehaviour
     {
         Debug.Log("Tap");
         TransformCamera();
+        ActivePanel();
     }
 
     private void TransformCamera()
@@ -22,5 +24,14 @@ public class TestButton : MonoBehaviour
         //camera.position = Vector3.Lerp(transform.position, thisGameObj.transform.position + offset, speed);
         camera.position = Vector3.Lerp(camera.transform.position, thisGameObj.transform.position, speed);
         
+    }
+
+    private void ActivePanel()
+    {
+        uiPanel.SetActive(true);
+    }
+     private void DeactivePanel()
+    {
+        uiPanel.SetActive(false);
     }
 }
