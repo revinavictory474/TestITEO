93b24fa3a891e86e55c2f769ecb363892c918ba6
Revina Victory <revina.victory@gmail.com>
2021-09-11 11:26:40 +0700

    test scripts

--- ITEO/Assets/Code/TestButton.cs
+++ ITEO/Assets/Code/TestButton.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TestButton : MonoBehaviour, IPointerDownHandler
+public class TestButton : MonoBehaviour
 {
-    public void OnMouseDown()
+    public GameObject thisGameObj;
+    public Transform camera;
+    private float speed = 0.03f;
+    public Vector3 offset;
+
+
+    public void OnMouseUp()
     {
         Debug.Log("Tap");
+        TransformCamera();
     }
 
-    public void OnDestroy()
+    private void TransformCamera()
     {
-        Destroy(gameObject);
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log(eventData.position);
+        //camera.position = Vector3.Lerp(transform.position, thisGameObj.transform.position + offset, speed);
+        camera.position = Vector3.Lerp(camera.transform.position, thisGameObj.transform.position, speed);
+        
     }
 }
